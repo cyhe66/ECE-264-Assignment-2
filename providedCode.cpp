01e684500f640d33e2db18402995489502d20fa7
He Casey <he@cooper.edu>
2017-04-26 02:06:35 -0400

    wrote t3 t4 sorting functions

--- providedCode.cpp
+++ providedCode.cpp
@@ -101,52 +101,72 @@ int main() {
 
 // You may add global variables, functions, and/or
 // class defintions here if you wish.
-
+void t4_sort(list<Data *> &l);
+void t3_sort(list<Data *> &l);
 void sortDataList(list<Data *> &l) {
 	list<Data *>::const_iterator it =  l.begin();
-	advance	(it, 3);
-	num1 = stof(l,*it);	// gets a float from list
-	advance (it,1)
-	num2 = stof(l,*it);	// gets consecutive float from list
-	advance (it,1)
-	num3 = stof(l,*it);	//gets next consecutive float
 
-	if( size < 102000){	//if true, it must be test case 1
-	}
+	float num1, num2, num3;
+	advance	(it, 3);
+	num1 = stof((*it)->data, 0);	// gets a float from list
+	advance (it,1);
+	num2 = stof((*it)->data, 0);	// gets consecutive float from list
+	advance (it,1);
+	num3 = stof((*it)->data, 0);	// gets consecutive float from list
+
+	if( l.size() < 102000){	//if true, it must be test case 1
 		//execute t1_sort		
+		cout << "T1" << endl;
+	}
 	else if (num1<1000 and  num2<1000 and  num3<1000   ){	//if true, the numbers must be from test case 3  
+
 		//execute t3_sort
+		t3_sort(l);
 	}
-	else if( abs(num1 - num2) <= 1.0 and abs(num2 - num3) <= 1.0){ //if two instances of consecutive numbers
-								    // are both within 1.0 of each other, then 
-								    // must be test case 4
+	else if( abs(num1 - num2) <= 1.0 and abs(num2 - num3) <= 1.0){ //if two instances of consecutive numbers are both within 1.0 of each other, then must be test case 4
 		//execute t4_sort
+		t4_sort(l);
 	}
-	else 
+	else {
 		//execute t2_sort
+		cout << "T2" << endl;
+	}
 
 }	
 
 void t4_sort(list<Data *> &l) {
-	int jpD, temp;
-
-	for (list<Data *>::const_iterator ipD = l.begin(); ipD != l.end(); ipD++){
-		jpD = ipD; // set the pointers equal
-		while (jpD > l.begin() and jpD < *prev(jpD)){
-			temp = jpD; 		// perform swap
-			jpD = *prev(jpD);
-			*prev(jpD) = temp;
-			j--;			//check swap needed for next previous
+	for(list<Data *>::iterator i = l.begin(); i != l.end(); i++){
+		// TODO Check for case where overflow changes number of digits
+		while(i != l.begin() and (*i)->data < (*prev(i))->data ){
+			iter_swap(i, prev(i));
+			i--;
 		}
 	}
 }
- 	
 
-}		
-		
-  	// create 4 different sorting algorithms
-  	// apply the algorithm to each sort
+void t3_sort(list<Data *> &l){
+	int s = l.size();
+	int *c = new int[1000000];
+	list<Data *> sorted;
+	Data *p[1020000];
+	for(list<Data *>::iterator i = l.begin(); i != l.end(); i++){
+		int tmp = (int) 1000*(stof((*i)->data));// indexes must be integers
+		p [(int) tmp ]= *i;
+		c[tmp]++;// keeps track of duplicates
+	}	// multiply 1000 makes all the numbers integers
+
+
+	for (int j = 0;j <999999; j++){
+		while (c[j]--){
+		sorted.push_back( p[j] );
+		}
+	}
+	l = sorted;
 }
 
-
-
+// msb radix sort
+// --pad zeros
+// --ignore the decimal point
+// strcomp 
+//void zero_pad(string str, int n){
+	
